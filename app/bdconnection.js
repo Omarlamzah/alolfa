63ee130b2621dc3242b606f36038c691c3d74303
Omarlamzah <102755164+Omarlamzah@users.noreply.github.com>
2023-04-03 00:10:32 +0000

    Create bdconnection.js
    change database conection

--- app/bdconnection.js
+++ app/bdconnection.js
@@ -6,4 +6,14 @@ password:'',
 database:'olfadb'
 });
 
-module.exports={connection}
+
+var connection2= mysql.createConnection({
+    host :'mysql-118987-0.cloudclusters.net',
+    user :'admin',
+    password:'r7HMwhfZ',
+    database:'olfadb',
+    port:12740
+    });
+
+
+module.exports={connection2}
